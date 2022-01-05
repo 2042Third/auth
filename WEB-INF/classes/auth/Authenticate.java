98d73ed3e7edb0987ebe04364ec38c377063c46b
mike yang <18604713262@163.com>
2022-01-05 08:52:26 -0500

    first

---
+++ WEB-INF/classes/auth/Authenticate.java
@@ -0,0 +1,90 @@
+package auth;
+import java.util.prefs.*;
+import java.net.URLDecoder;
+import java.sql.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Scanner;
+import java.io.File;
+
+public class Authenticate {
+    private Connection con;
+    
+    public Boolean login (String url, String user, String a){
+        try{
+            Class.forName("org.mariadb.jdbc.Driver");
+        }
+        catch (Exception e){
+            System.out.println("mariadb driver not found.");
+        }
+        if (con!=null)
+            logout();
+        try {
+            con = DriverManager.getConnection(
+                        url, 
+                        user, 
+                        a
+                    );
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[file tracker storage] login failure");
+            return false;
+        }
+        System.out.println("[file tracker storage] login success");
+        return true;
+    }
+
+    public Boolean exc_emplace_5 (String query, String a1,String a2,String a3,String a4,String a5){
+        try { 
+            PreparedStatement stat = con.prepareStatement(query);
+            stat.setString(1, a1);
+            stat.setString(2, a2);
+            stat.setString(3, a3);
+            stat.setString(4, a4);
+            stat.setString(5, a5);
+            stat.executeQuery();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[file tracker storage] query failure \""+query+"\"");
+            return false;
+        }
+        
+        System.out.println("[file tracker storage] query sucess \""+query+"\"");
+        return true;
+    }
+
+    private Boolean exc (String a) {
+        try{
+            PreparedStatement stat = con.prepareStatement(a);
+            ResultSet rs = stat.executeQuery();
+            System.out.println("[file tracker storage] query excuted \""+a+"\"");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[file tracker storage] query failure \""+a+"\"");
+            return false;
+        }
+        
+        return true;
+    }
+
+    public Boolean logout (){
+        if (con != null) {
+          try {
+            con.close();
+          }
+          catch (SQLException ex) {
+            for (Throwable t : ex)
+              System.out.println(t.getMessage());
+            System.out.println("[file tracker storage] Closing connection unsuccessful!");
+            return false;
+          
+          }
+          return true;
+        }
+        return false;
+    }
+}
