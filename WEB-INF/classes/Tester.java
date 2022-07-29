7a701fbf75c89ef3bfbfb132bec1ddf2a3fa4ffd
mike yang <18604713262@163.com>
2022-07-29 19:20:59 -0400

    Native calls to C

---
+++ WEB-INF/classes/Tester.java
@@ -0,0 +1,6 @@
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println("Doing");
+//        new EncryptionTools().pp_hash_wrap();  // Invoke native method
+    }
+}
--- WEB-INF/classes/security/EncryptionTools.java
+++ WEB-INF/classes/security/EncryptionTools.java
@@ -5,4 +5,9 @@ public class EncryptionTools {
         System.loadLibrary("cc20"); // loads the encryption module
     }
     public native void pp_hash_wrap(); // the function in c
+    // Test Driver
+    public static void main(String[] args) {
+        System.out.println("Doing");
+//        new EncryptionTools().pp_hash_wrap();  // Invoke native method
+    }
 }
